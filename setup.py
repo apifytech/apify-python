77337642c9bcffa7086bcbffa0a4db2ef2083a0c
Jan Curn <jan.curn@gmail.com>
2019-01-08 20:11:51 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Work in progress: Apify SDK for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://sdk.apify.com",
+    url="https://github.com/apifytech/apify-python",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
