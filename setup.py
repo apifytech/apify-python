40a56d78c44802f700e44fa2062630ee656caffd
Jan Curn <jan.curn@gmail.com>
2019-01-08 20:09:43 +0100

    Initial commit

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="apify_client",
+    name="apify",
     version="0.0.1",
     author="Apify Technologies s.r.o.",
     author_email="support@apify.com",
-    description="Work in progress: Apify API client for Python",
+    description="Work in progress: Apify SDK for Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://sdk.apify.com",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache License 2.0",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
